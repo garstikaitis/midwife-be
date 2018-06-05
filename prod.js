62bada8f1ce391de3f20cdc5dda3b3145d662cf2
Gintaras Arstikaitis <ga@companyoung.com>
2018-06-05 18:03:58 +0200

    production config

---
+++ prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  production: {
+    username: 'bbe73983f1c94f',
+    password: 'cdd31008',
+    database: 'heroku_c891aa4219bdd6f',
+    host: 'eu-cdbr-west-02.cleardb.net',
+    dialect: 'mysql',
+  },
+};
--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,28 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('midwife', 'root', 'rootroot', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+const sequelize =
+  process.env !== 'production'
+    ? new Sequelize('midwife', 'root', 'rootroot', {
+        host: 'localhost',
+        dialect: 'mysql',
+        operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+      })
+    : new Sequelize('heroku_c891aa4219bdd6f', 'bbe73983f1c94f', 'cdd31008', {
+        host: 'eu-cdbr-west-02.cleardb.net',
+        dialect: 'mysql',
+        operatorsAliases: false,
 
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+      });
 module.exports = sequelize;
